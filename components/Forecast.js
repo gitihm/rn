7b49d40a2b8c9ecb0fe07a4d1f59c1562a1c07df
gitihm <ismael897@outlook.com>
2018-09-28 04:18:35 +0700

    show text 'main des temp'

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View } from 'react-native';
+import {Text, View ,StyleSheet} from 'react-native';
 import { black } from 'ansi-colors';
 export default class Forecast extends React.Component {
     render() {
@@ -7,20 +7,18 @@ export default class Forecast extends React.Component {
             
             <View style={{flex:1,flexDirection: 'column',alignItems:'center'}}>
                 <View style={{alignSelf:'stretch',height:250,backgroundColor:'black',opacity:.5}}>
-
+                    <Text style= {styles.text2}>{"\n"}{this.props.main}</Text>
+                    <Text style= {styles.text3}>{"\n"}{this.props.description}</Text>
+                    <Text style= {styles.text4}>{"\n"}{this.props.temp} °C</Text>
                 </View>
             
-            </View>
-            
-            
-            
-            
-
-
-
-            
-               
-            
+            </View>              
         );
     }
 }
+const styles = StyleSheet.create({
+    text1: {textAlign: 'center' , fontSize: 20 , color:'white'},
+    text2: {textAlign: 'center' , fontSize: 30 , color:'white'},
+    text3: {textAlign: 'center' , fontSize: 18 , color:'white'},
+    text4: {textAlign: 'center' , fontSize: 28 , color:'white'},
+  });
