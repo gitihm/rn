bf2db135a83584c5795b897d0e9451c464d832d7
gitihm <ismael897@outlook.com>
2018-09-28 04:16:21 +0700

    create flex

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {Text, View } from 'react-native';
+import { black } from 'ansi-colors';
 export default class Forecast extends React.Component {
     render() {
         return (
-            <View>
-                <Text>{this.props.main}</Text>
-                <Text>{this.props.description}</Text>
-                <Text>{this.props.temp}</Text>
-                <Text>°C</Text>
+            
+            <View style={{flex:1,flexDirection: 'column',alignItems:'center'}}>
+                <View style={{alignSelf:'stretch',height:250,backgroundColor:'black',opacity:.5}}>
+
+                </View>
+            
             </View>
+            
+            
+            
+            
+
+
+
+            
+               
+            
         );
     }
 }
