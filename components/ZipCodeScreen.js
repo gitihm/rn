1a0d9a10da5a9f0da8254b2be59fbb704ea25149
gitihm <ismael897@outlook.com>
2018-09-28 13:19:51 +0700

    add code  zipcode screen

--- components/ZipCodeScreen.js
+++ components/ZipCodeScreen.js
@@ -14,3 +14,26 @@ const ZipItem = ({place, code, navigate}) => (
       <Text style={styles.zipCode}>{code}</Text>
     </View>
   )
+
+ 
+const _keyExtractor = item => item.code
+
+export default class WeatherScreen extends React.Component {
+  static navigationOptions = ({navigation}) => {
+    return {
+        headerTitle: (<Text>Choose a zip code</Text>),
+    }
+  }
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <FlatList
+          data={availableZipItems}
+          keyExtractor={_keyExtractor}
+          renderItem={({item}) => <ZipItem {...item} navigate={navigate}/>}
+        />
+      </View>
+    );
+  }
+}
