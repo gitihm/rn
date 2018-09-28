98a96b1dab157db0b8be0412153c157b1c6baac6
gitihm <ismael897@outlook.com>
2018-09-28 13:22:44 +0700

    add All code in sheet but error

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Weather from './components/Weather'
+import { createStackNavigator } from 'react-navigation';
 
 export default class WeatherScreen extends React.Component {  
   static navigationOptions = ({navigation}) => {
       return {
           headerTitle: (<Text>Weather</Text>),
       }
+      const RootStack = createStackNavigator({
+        Weather: WeatherScreen,
+        ZipCode: ZipCodeScreen
+      },{
+        initialRouteName: 'Weather',
+      })
+      
   }
+  
 
   render() {
     return (
-      <Weather zipCode="90110"/>
+      <RootStack/>
     );
   }
 }
